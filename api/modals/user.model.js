d30eef38f6a472f932a176dfeeca20464c7386e1
sahil <s.s.y.9321423@gmail.com>
2024-12-23 08:16:57 +0530

    UserSchema

---
+++ api/modals/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: 'string',
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 20
+  }, 
+  email: {
+    type: 'string',
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  password: {
+    type: 'string',
+    required: true,
+    minlength: 6
+  },
+},
+{
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
