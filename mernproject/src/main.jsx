91a4d9d00cbfb4b5ae3c22e28e1adc0258111c2e
sahil <s.s.y.9321423@gmail.com>
2024-12-24 10:29:53 +0530

    add redux-persistl

--- mernproject/src/main.jsx
+++ mernproject/src/main.jsx
@@ -3,9 +3,13 @@ import './index.css';
 import App from './App.jsx';
 import { store } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { persistor } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
   <Provider store={store}>
     <App />
   </Provider>
+  </PersistGate>
 );
