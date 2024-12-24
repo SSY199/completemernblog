afa154625ef006ef609ca816011932cf0d4a2ed2
sahil <s.s.y.9321423@gmail.com>
2024-12-24 15:28:54 +0530

    make dashboard private

--- mernproject/src/App.jsx
+++ mernproject/src/App.jsx
@@ -1,12 +1,14 @@
+//import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Dashboard from "./Pages/Dashboard";
-import Home from "./Pages/Home";
+import Projects from "./Pages/Projects";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
-import Projects from "./Pages/Projects";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
   return (
@@ -15,10 +17,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
