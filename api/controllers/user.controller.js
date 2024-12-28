98cfcb5ba3a8bd13ecf85dd560831c69f1ab60ba
sahil <s.s.y.9321423@gmail.com>
2024-12-28 12:57:04 +0530

    Created getUser api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -67,3 +67,36 @@ export const signout = (req, res, next) => {
     next(error);
   }
 };
+
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You do not have permission to view all users"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+    const now = Date.now();
+    const oneMonthAgo = new Date(now - 30 * 24 * 60 * 60 * 1000);
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({ users: usersWithoutPassword, totalUsers, lastMonthUsers });
+  } catch (error) {
+    next(error);
+  }
+};
