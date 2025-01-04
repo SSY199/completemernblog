bd7076266a63df0ac273db32dee84a2f20f32c76
sahil <s.s.y.9321423@gmail.com>
2025-01-04 20:32:08 +0530

    final push

--- mernproject/src/supabase.js
+++ mernproject/src/supabase.js
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fgasxvvwqrwixtmkueyo.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY; // Use VITE_ prefix for environment variables in Vite
-const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabaseUrl = 'https://fgasxvvwqrwixtmkueyo.supabase.co';
+// const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY; // Use VITE_ prefix for environment variables in Vite
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+// export default supabase;
